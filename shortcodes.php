1c18b526c9e83fe5424259a998048df8d0eab15b
Shannon MacMillan <shannon@webdevstudios.com>
2017-02-16 05:37:52 -0700

    Start shortcodes file and copyright function

--- shortcodes.php
+++ shortcodes.php
@@ -2,4 +2,15 @@
 /** SHORTCODES
 * This file includes NGNG shortcodes for the child theme.
 *
-* /
+*/
+
+
+/** Copyright shortcode
+* Add an incremental copyright with the site title, for use in wp editor.
+*
+* @author Shannon MacMillan
+*/
+function ngng_do_copyright() {
+
+}
+add_shortcode( 'copyright', 'ngng_do_copyright' );
